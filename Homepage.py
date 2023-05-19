ab224362fcb84c6db1790cc2f2eec17b2bc94371
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-19 22:02:36 +0700

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Website Kombiansi AHC & K-Means",
+    page_icon="👋🏻",
+)
+
+st.title("Home Page")
+st.sidebar.success("Pilih halaman diatas.")
