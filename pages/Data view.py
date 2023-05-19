add96815da5e9c0c1134af0527a65b7e1a3096f3
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-19 23:09:10 +0700

    Update Data view.py

--- pages/Data view.py
+++ pages/Data view.py
@@ -16,13 +16,13 @@ def save_upload(uploadedfile):
         f.write(uploadedfile.getbuffer())
         return st.success("File berhasil disave: {} in Documents".format(uploadedfile.name))
 
-data_file = st.file_uploader("Upload Excel",type=["xlsx"])
+data_file = st.file_uploader("Upload Excel",type=["excel"])
 if data_file is not None:
     st.write(type(data_file))
     file_details = {"Filename":data_file.name,
     "FileType":data_file.type,"FileSize":data_file.size}
     st.write(file_details)
-    df = pd.read_xlsx(data_file)
+    df = pd.read_excel(data_file)
     st.dataframe(df)
 
     # Save File
