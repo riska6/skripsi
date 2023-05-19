ddfd169647c6651834454b7a987ba47989d886cf
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-19 22:03:37 +0700

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,8 +1,18 @@
 import streamlit as st
+
 st.set_page_config(
-    page_title="Website Kombiansi AHC & K-Means",
-    page_icon="👋🏻",
+    page_title="Multipage App",
+    page_icon="👋",
 )
 
-st.title("Home Page")
+st.title("Main Page")
 st.sidebar.success("Pilih halaman diatas.")
+
+if "my_input" not in st.session_state:
+    st.session_state["my_input"] = ""
+
+my_input = st.text_input("Input a text here", st.session_state["my_input"])
+submit = st.button("Submit")
+if submit:
+    st.session_state["my_input"] = my_input
+    st.write("You have entered: ", my_input)
