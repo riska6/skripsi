139d9df6af390cc53131d268df647ec7ecd7354e
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-19 23:04:40 +0700

    Update Data view.py

--- pages/Data view.py
+++ pages/Data view.py
@@ -3,14 +3,10 @@ import streamlit as st
 import pandas as pd
 import os
 
-st.subheader("Halaman CSV")
+st.subheader("Halaman Excel")
 st.write(
             """
-            Di dalam Halaman CSV akan mempelajari bagaimana caranya Upload file dan Membaca File dengan _type CSV_.
-            
-            Dalam tutorial ini sebenernya sudah pernah kita bahas 
-            di [video](https://www.youtube.com/watch?v=SX-4STwpFQA&lc=Ugw6TKYhoPmV0bVyHFl4AaABAg) 
-            dan ini adalah materi lanjutannya 
+            Pada halaman ini, akan menampilkan bagaimana data pasien yang akan digunakan pada website.
             """
         )
 
@@ -20,13 +16,13 @@ def save_upload(uploadedfile):
         f.write(uploadedfile.getbuffer())
         return st.success("File berhasil disave: {} in Documents".format(uploadedfile.name))
 
-data_file = st.file_uploader("Upload CSV",type=["csv"])
+data_file = st.file_uploader("Upload Excel",type=["xlsx"])
 if data_file is not None:
     st.write(type(data_file))
     file_details = {"Filename":data_file.name,
     "FileType":data_file.type,"FileSize":data_file.size}
     st.write(file_details)
-    df = pd.read_csv(data_file)
+    df = pd.read_xlsx(data_file)
     st.dataframe(df)
 
     # Save File
