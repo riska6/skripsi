9b3f8b233e497b64f651b6ebf504307cdf4b85ae
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-19 21:51:35 +0700

    Delete Homepage.py

--- page/Homepage.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.set_page_config(
-    page_title="Website Kombiansi AHC & K-Means",
-    page_icon="👋🏻",
-)
-
-st.title("Home Page")
-st.sidebar.success("Pilih halaman diatas.")
