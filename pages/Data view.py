c1e7befc937ef2693718859571bcc33f5aa54bd7
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-19 23:00:15 +0700

    Update and rename dataview.py to Data view.py

--- pages/Data view.py
+++ pages/Data view.py
@@ -20,14 +20,13 @@ def save_upload(uploadedfile):
         f.write(uploadedfile.getbuffer())
         return st.success("File berhasil disave: {} in Documents".format(uploadedfile.name))
 
-data_file = st.file_uploader("Upload CSV/EXCEL",type=["csv"], type=["xlsx"])
+data_file = st.file_uploader("Upload CSV",type=["csv"])
 if data_file is not None:
     st.write(type(data_file))
     file_details = {"Filename":data_file.name,
     "FileType":data_file.type,"FileSize":data_file.size}
     st.write(file_details)
     df = pd.read_csv(data_file)
-    df = pd.read_xlsx(data_file)
     st.dataframe(df)
 
     # Save File
