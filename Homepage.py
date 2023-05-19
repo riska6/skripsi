7f3d176247f93a382699a3fdbce3883ec48c40be
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-19 22:40:18 +0700

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import webbrowser
 
- st.write("url = 'https://riska6-skripsi-homepage-dgg9jf.streamlit.app/contact")
-
-if st.button('Open browser'):
-    webbrowser.open_new_tab(url)
-
 st.set_page_config(
     page_title="Selamat Datang di Website Kombinasi AHC & K-Means",
     page_icon="👋",
