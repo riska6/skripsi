3640d77247f09d416223c0d53afb7ad3fb52cfa3
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-19 22:18:54 +0700

    Update projects.py

--- page/projects.py
+++ page/projects.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 
-st.title("Projects")
+st.title("View Data")
 
-st.write("You have entered", st.session_state["my_input"])
+st.write("Pengelompokan data pasien yang dilakukan pada suatu instansi kesehatan masih belum secara total menganalisa data yang memiliki karakteristik gejala dengan tingkat Diabetes yang mirip. Untuk mengetahui apakah pengelompokan dengan karakteristik yang mirip menggunakan data pasien, dilakukan pengelompokan data menggunakan penggabungan lebih dari satu metode, yaitu metode K-Means dan AHC (Agglomerative Hierarchical Clustering). Berikut adalah data pasien tersebut:", st.session_state["my_input"])
