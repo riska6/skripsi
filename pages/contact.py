8f9d1bcf9f90dfcf4d3449e5b138011896eb837d
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-19 22:26:53 +0700

    Update contact.py

--- pages/contact.py
+++ pages/contact.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-st.title("Contact")
+
+st.title("View Data")
+
+st.write("Pengelompokan data pasien yang dilakukan pada suatu instansi kesehatan masih belum secara total menganalisa data yang memiliki karakteristik gejala dengan tingkat Diabetes yang mirip. Untuk mengetahui apakah pengelompokan dengan karakteristik yang mirip menggunakan data pasien, dilakukan pengelompokan data menggunakan penggabungan lebih dari satu metode, yaitu metode K-Means dan AHC (Agglomerative Hierarchical Clustering). Berikut adalah data pasien Diabetes yang berasal dari Puskesmas Modopuro, Kec. Mojosari, Kab. Mojokerto.", st.session_state["my_input"])
