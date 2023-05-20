e84fccd1f16f5fc59e586c3ebc61b9e47334f34f
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-20 13:15:12 +0700

    Update Data view.py

--- pages/Data view.py
+++ pages/Data view.py
@@ -10,9 +10,21 @@ st.write(
             Pada halaman ini, akan menampilkan bagaimana data pasien yang akan digunakan pada website.
             """
         )
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  df = pd.read_excel(uploaded_file)
+  st.write(df)
 
-df = pd.read_csv("./data/titanic.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
-# df = pd.read_excel(...)  # will work for Excel files
-
-st.title("Hello world!")  # add a title
-st.write(df)  # visualize my dataframe in the Streamlit app
+  # Add some matplotlib code !
+  fig, ax = plt.subplots()
+  df.hist(
+    bins=8,
+    column="Age",
+    grid=False,
+    figsize=(8, 8),
+    color="#86bf91",
+    zorder=2,
+    rwidth=0.9,
+    ax=ax,
+  )
+  st.write(fig)
