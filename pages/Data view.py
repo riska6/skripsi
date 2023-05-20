e6c68756b0895317ef2677f83bb48e87314fca29
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-20 13:21:02 +0700

    Update Data view.py

--- pages/Data view.py
+++ pages/Data view.py
@@ -14,17 +14,4 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_excel(uploaded_file)
   st.write(df)
-
-  # Add some matplotlib code !
-  fig, ax = plt.subplots()
-  df.hist(
-    bins=8,
-    column="Age",
-    grid=False,
-    figsize=(8, 8),
-    color="#86bf91",
-    zorder=2,
-    rwidth=0.9,
-    ax=ax,
-  )
   st.write(fig)
