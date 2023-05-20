fbc20d0c47700468dd8a9a28e0cdf606f739a61b
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-20 14:39:05 +0700

    Update View Data.py

--- pages/View Data.py
+++ pages/View Data.py
@@ -7,4 +7,20 @@ st.title("View Data")
 st.sidebar.subheader("Pengaturan Visualisasi")
 
 #setup file upload
-st.sidebar.file_uploader(label="Upload your excel or csv file.")
+upload_file = st.sidebar.file_uploader(label="Upload your excel or csv file.",
+                        type=['csv', 'xlsx'])
+global df 
+if uploaded_file is not None:
+  print(uploaded_file)
+  print("hello")
+  try:
+    df = pd.read_excel(uploaded_file)
+  except Exception as e:
+    print(e)
+    df = pd.read_csv(uploaded_file)
+    
+try:
+  st.write(df)
+except Exception as e:
+  print(e)
+  st.write("Tolong upload file yang benar.")
