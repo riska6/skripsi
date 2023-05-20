56380c65b8520dc6867539fa40bde4a3bcf9e7c2
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-20 14:08:00 +0700

    Update View Data.py

--- pages/View Data.py
+++ pages/View Data.py
@@ -9,5 +9,5 @@ st.write(
             Pada halaman ini, akan menampilkan bagaimana data pasien yang akan digunakan pada website.
             """
         )
-df = pd.read_excel("./pages/DATA PASIEN DIABETES FIX.xlsx")  # read a CSV file inside the 'data" folder next to 'app.py'
+df = pd.read_excel("./pages/DATA PASIEN DIABETES FIX.xlsx")  # read a excel file inside the 'data" folder next to 'app.py'
 # df = pd.read_excel(...)  # will work for Excel files
