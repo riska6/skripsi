eb99a55de1f1ba333e7ff61b4a7770130ff416db
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-20 14:23:13 +0700

    Update View Data.py

--- pages/View Data.py
+++ pages/View Data.py
@@ -1,6 +1,7 @@
 # Import Library
 import streamlit as st
 import pandas as pd
+from streamlit_option_menu import option_menu
 import os
 
 st.title("View Data")
@@ -9,6 +10,11 @@ st.write(
             Pada halaman ini, akan menampilkan bagaimana data pasien yang akan digunakan pada website.
             """
         )
-df = pd.read_excel("./pages/DATA PASIEN DIABETES FIX.xlsx")  # read a excel file inside the 'data" folder next to 'app.py'
-# df = pd.read_excel(...)  # will work for Excel files
-st.write(df)
+
+with st.sidebar:
+    selected = option_menu("Product", ["BFR CORPORATE", 'BFR mikro', 'BFR Consumer', 'BRF'], 
+        icons=['play', 'play'], menu_icon="cast", default_index=1)
+    selected
+    print(selected)
+df = pd.read_excel("DATA PASIEN DIABETES FIX.xlsx")
+st.dataframe(df)
