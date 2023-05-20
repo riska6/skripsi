802062c7f7cdd37e13b06cde17c40c0ab3aec671
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-20 15:30:14 +0700

    Update View Data.py

--- pages/View Data.py
+++ pages/View Data.py
@@ -14,7 +14,7 @@ if uploaded_file is not None:
   print(uploaded_file)
   print("hello")
   try:
-    df = pd.read_excel(uploaded_file)
+    df = pd.read_csv(uploaded_file)
   except Exception as e:
     print(e)
     df = pd.read_excel(uploaded_file)
