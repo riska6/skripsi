a267517812a403775b35d1763e5910fb9e8ed634
riska6 <72201217+riska6@users.noreply.github.com>
2023-05-20 13:28:53 +0700

    Update Data view.py

--- pages/Data view.py
+++ pages/Data view.py
@@ -13,5 +13,4 @@ st.write(
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_excel(uploaded_file)
-  st.write(df)
-  st.write(fig)
+  st.write(dataframe)
